30c57bfab4aadd319cad6d63929a615e83e868b4
Edu Note <aragonenred@gmail.com>
2022-08-01 23:23:52 -0300

    Ultimocomit

--- src/app/components/alta-alumno/alta-alumno.component.ts
+++ src/app/components/alta-alumno/alta-alumno.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-alta-alumno',
@@ -19,10 +20,48 @@ export class AltaAlumnoComponent implements OnInit {
 
    }
 
+   previsualizacion!: string;
+
    guardarAlumno(){
 
     console.log(this.formulario);
    }
+
+   capturarFile(event:any){
+      console.log(event.target.files[0]);
+
+       this.cargarPrevisualizacion(event.target.files[0]);
+   }
+
+
+
+   cargarPrevisualizacion(file: File) {
+    var base64:string;
+    const observable = new Observable((subscriber: Subscriber<any>) => {
+      this.readFile(file, subscriber);
+      console.log('paso 2');
+    })
+
+    observable.subscribe((d) => {
+      this.previsualizacion = d;
+    });
+
+   }
+
+   readFile(file:File, subscriber: Subscriber<any>){
+     const filereader = new FileReader();
+     filereader.readAsDataURL(file)
+     filereader.onload = () =>{
+       subscriber.next(filereader.result);
+       subscriber.complete()
+     }
+     filereader.onerror = ()=>{
+       subscriber.error()
+       subscriber.complete()
+     }
+   }
+
+
 
   ngOnInit(): void {
   }

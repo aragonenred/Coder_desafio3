99fbaddb41f05ce4293bfacca115e31c0f2bbc0d
Aragon en Red <aragonenred@gmail.com>
2022-08-04 01:11:53 -0300

    Final Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { AltaAlumnoComponent } from './components/alta-alumno/alta-alumno.component';
+
 import { AlumnosComponent } from './components/alumnos/alumnos.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AltaAlumnoComponent,
     AlumnosComponent
   ],
   imports: [
